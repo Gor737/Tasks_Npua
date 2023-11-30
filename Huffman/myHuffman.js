4cb89338d73e81a1c6ae8a50c86240d74dc86f1a
Gor Afyan <118358485+Gor737@users.noreply.github.com>
2023-11-30 10:24:23 +0400

    Update myHuffman.js

--- Huffman/myHuffman.js
+++ Huffman/myHuffman.js
@@ -1,3 +1,7 @@
+                                                   //Variant 1
+
+
+
 class HuffmanNode {
     constructor(ch,  frequency,  left,   right) {
         this.ch = ch;
@@ -85,3 +89,86 @@ var encoded = huffman.encode(codeMap, input);
 console.log("encoding string: " + encoded); 
 var decode = huffman.decode(encoded);
 console.log("decoding string: " + decode);
+
+
+
+
+
+
+                                                   //Variant  2
+
+
+class Node {
+    constructor(value, char = '') {
+        this.value = value;
+        this.char = char;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildHuffmanTree(freq) {
+    let nodes = [];
+
+    for (let char in freq) {
+        nodes.push(new Node(freq[char], char));
+    }
+
+    while (nodes.length > 1) {
+        nodes.sort((a, b) => a.value - b.value);
+
+        let left = nodes.shift();
+        let right = nodes.shift();
+
+        let newNode = new Node(left.value + right.value);
+        newNode.left = left;
+        newNode.right = right;
+
+        nodes.push(newNode);
+    }
+
+    return nodes[0];
+}
+
+function generateCodes(root, prefix = '', codes = {}) {
+    if (root) {
+        if (root.char !== '') {
+            codes[root.char] = prefix;
+        }
+        generateCodes(root.left, prefix + '0', codes);
+        generateCodes(root.right, prefix + '1', codes);
+    }
+    return codes;
+}
+
+function huffmanEncoding(text) {
+    let freq = {};
+
+    for (let char of text) {
+        if (freq[char]) {
+            freq[char]++;
+        } else {
+            freq[char] = 1;
+        }
+    }
+
+    let root = buildHuffmanTree(freq);
+    let codes = generateCodes(root);
+
+    let encodedText = '';
+    for (let char of text) {
+        encodedText += codes[char];
+    }
+
+    return {
+        encodedText: encodedText,
+        codes: codes
+    };
+}
+
+let text = 'Gor Afyan';
+let encodedResult = huffmanEncoding(text);
+
+console.log('Original Text:', text);
+console.log('Encoded Text:', encodedResult.encodedText);
+console.log('Huffman Codes:', encodedResult.codes);
