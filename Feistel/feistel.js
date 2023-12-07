0d19224664278215a91946e83e455896862c13d9
Gor Afyan <118358485+Gor737@users.noreply.github.com>
2023-12-07 09:57:00 +0400

    Update feistel.js

--- Feistel/feistel.js
+++ Feistel/feistel.js
@@ -1,26 +1,4 @@
-/**
- * This function implements a Feistel Cipher with 4 rounds, where the block size is 16 bits and the key size is 16 bits.
- * The Feistel function uses the following S-box: 
- * 0 1 2 3 4 5 6 7 8 9 A B C D E F 
- * 6 7 8 9 A B C D E F 0 1 2 3 4 5 
- * The key is divided into 2 8-bit subkeys, K1 and K2, where K1 is the leftmost 8 bits and K2 is the rightmost 8 bits, alternating every round.
- * The Feistel cipher with 4 rounds can be described as follows:
- * 1. Divide the 16-bit plaintext block P into two 8-bit halves, L0 and R0.
- * 2. For i = 1 to 4, do the following:
- *    - Calculate Li = Ri-1.
- *    - Calculate Ri = Li-1 ^ F(Ri-1, Ki), where F is a round function that takes Ri-1 as input and produces a 8-bit output, and Ki is the ith subkey.
- *    - The round function XORs the Ri-1 and Ki then substitutes the 8-bits using the S-Box.
- *    - Example:
- *      - Ri-1 = 10101100
- *      - Ki = 10101010
- *      - XORed = 00000110
- *      - Substituted using S-Box 0000->0110, 0110->1100, thus 01101100
- * 
- * @param {string} plaintext - The plaintext to be encrypted or decrypted. Must be a 16-bit binary string.
- * @param {string} key - The key to be used for encryption or decryption. Must be a 16-bit binary string.
- * @param {string} mode - The mode of operation. Must be either "encrypt" or "decrypt".
- * @returns {string} - The encrypted or decrypted result as a 16-bit binary string.
- */
+
 function feistelCipher(plaintext, key, mode) {
   try {
     // Check if plaintext and key are 16-bit binary strings
